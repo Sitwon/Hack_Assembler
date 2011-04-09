27ec19fc7e97e9311d3c3224dd19f4fec35a7412
Ben Mendis <dragonwisard@gmail.com>
2011-04-09 00:18:00 -0400

    Adding the Main class, our entry point

---
+++ src/org/hacdc/tecs/project_6/assembler/Main.java
@@ -0,0 +1,20 @@
+/**
+ * An object-oriented solution to the Chapter 6 project using Java.
+ *
+ * @author		Ben "the Pyrate" Mendis
+ */
+package org.hacdc.tecs.project_6.assembler;
+
+public class Main {
+	/**
+	 * The entry-point for the application.
+	 *
+	 * Usage: java assembler.jar &lt;input file&gt;
+	 *
+	 * @param args The command-line arguments.
+	 */
+	public static void main (String[] args) {
+		System.err.println("Nothing to see here, yet.");
+	}
+}
+
