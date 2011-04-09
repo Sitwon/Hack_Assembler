f8a9c31574f3b659fea90d2b8b910aa28dbaabdb
Ben Mendis <dragonwisard@gmail.com>
2011-04-09 02:39:06 -0400

    Adding a CommandFactory to create command objects

---
+++ src/org/hacdc/tecs/project_6/assembler/CommandFactory.java
@@ -0,0 +1,20 @@
+/**
+ * A Factory for generating Command objects.
+ *
+ * This class is responsible for generating an appropriate Command object for
+ * the current command string.
+ *
+ * @author Ben "the Pyrate" Mendis
+ */
+
+package org.hacdc.tecs.project_6.assembler;
+
+public class CommandFactory {
+	public static Command getCommand (String command) {
+		if (command.startsWith("@")) {
+			return (Command) new ACommand(command);
+		}
+		return (Command) new CCommand(command);
+	}
+}
+
