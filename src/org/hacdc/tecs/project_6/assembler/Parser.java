df07299a09ea85f9b4b90261653c5f716927fd80
Ben Mendis <dragonwisard@gmail.com>
2011-04-09 00:38:22 -0400

    Adding the Parser class

---
+++ src/org/hacdc/tecs/project_6/assembler/Parser.java
@@ -0,0 +1,27 @@
+/**
+ * Encapsulates access to the input code.
+ *
+ * Encapsulates access to the input code. Reads an assembly language command,
+ * parses it, and provides convenient access to the command's components
+ * (fields and symbols). In addition, removes all white space and comments.
+ *
+ * @author Ben "the Pyrate" Mendis
+ */
+
+package org.hacdc.tecs.project_6.assembler;
+
+import java.io.InputStream;
+
+public class Parser {
+	private final InputStream input;
+
+	/**
+	 * Constructor.
+	 *
+	 * @args input An InputStream containing the assembly language commands.
+	 */
+	public Parser (InputStream input) {
+		this.input = input;
+	}
+}
+
