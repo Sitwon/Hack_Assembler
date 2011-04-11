56d471dd1fd6d9ef563d1129b1f265508509813c
Ben Mendis <dragonwisard@gmail.com>
2011-04-10 20:09:45 -0400

    Filling in Main so we can actually run the assembler
    In this single-pass version, it now works correctly for symbol-less
    assembly programs.

--- src/org/hacdc/tecs/project_6/assembler/Main.java
+++ src/org/hacdc/tecs/project_6/assembler/Main.java
@@ -3,9 +3,14 @@
  *
  * @author		Ben "the Pyrate" Mendis
  */
+
 package org.hacdc.tecs.project_6.assembler;
 
+import java.io.*;
+
 public class Main {
+	protected static Code code = new Code();
+
 	/**
 	 * The entry-point for the application.
 	 *
@@ -14,7 +19,14 @@ public class Main {
 	 * @param args The command-line arguments.
 	 */
 	public static void main (String[] args) {
-		System.err.println("Nothing to see here, yet.");
+		try {
+			Parser parser = new Parser(new FileInputStream(args[0]));
+			while (parser.hasMoreCommands()) {
+				System.out.println(code.translate(parser.nextCommand()));
+			}
+		} catch (FileNotFoundException ex) {
+			System.err.println("Error: Input file was not found.");
+		}
 	}
 }
 
