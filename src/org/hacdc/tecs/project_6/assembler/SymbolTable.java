59f2639ecf30e2eb23b10c29c32c87d0e6337ec2
Ben Mendis <dragonwisard@gmail.com>
2011-04-12 13:46:44 -0400

    Adding some more javadoc comments

--- src/org/hacdc/tecs/project_6/assembler/SymbolTable.java
+++ src/org/hacdc/tecs/project_6/assembler/SymbolTable.java
@@ -11,6 +11,11 @@ import java.util.*;
 public class SymbolTable extends HashMap<String, Integer> {
 	protected int nextAddress = 16;
 
+	/**
+	 * Constructor for SymbolTable.
+	 *
+	 * Initializes the SymbolTable and sets up the predefined symbols.
+	 */
 	public SymbolTable () {
 		super();
 		this.put("SP", 0);
@@ -25,6 +30,15 @@ public class SymbolTable extends HashMap<String, Integer> {
 		this.put("KBD", 24576);
 	}
 
+	/**
+	 * Register a new variable in the table.
+	 *
+	 * Register a new variable in the table. If the variable is already registered,
+	 * return the existing address.
+	 *
+	 * @param symbol The new variable to be registered.
+	 * @return The address of the registered variable.
+	 */
 	public int registerVariable (String symbol) {
 		if (this.contains(symbol)) {
 			return this.getAddress(symbol);
@@ -33,14 +47,31 @@ public class SymbolTable extends HashMap<String, Integer> {
 		return this.nextAddress++;
 	}
 
+	/**
+	 * Add and entry into the table.
+	 *
+	 * @param symbol The new symbol to be added.
+	 * @param address The corresponding address for the new symbol.
+	 */
 	public void addEntry (String symbol, Integer address) {
 		this.put(symbol, address);
 	}
 
+	/**
+	 * Check if a symbol exists in the table.
+	 *
+	 * @return True if the symbol exists in the table, otherwise False.
+	 */
 	public boolean contains (String symbol) {
 		return this.containsKey(symbol);
 	}
 
+	/**
+	 * Look up a symbol's address.
+	 *
+	 * @param symbol The symbol to look up.
+	 * @return The corresponding address.
+	 */
 	public Integer getAddress (String symbol) {
 		return this.get(symbol);
 	}
